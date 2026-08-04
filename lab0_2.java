5e966a13b6213ff6f01c6bf90b93549a2b136a28
Prit-Patel08 <pritpatel102008@gmail.com>
2026-08-04 18:41:00 +0530

    Create lab0_2.java

---
+++ lab0_2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+        int count = 0;
+        int num = 2;
+        while (count < n) {
+            boolean isPrime = true;
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.print(num + " ");
+                count++;
+            }
+            num++;
+        }
+        sc.close();
+    }
+}
